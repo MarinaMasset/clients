51ee70d09b72e2159d0f15ed8e337e1e54994852
Marina Masset <masset.marina@live.fr>
2022-11-08 17:07:08 +0100

    listing/recherche/tri

---
+++ front/src/components/Client.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//Le composant fils (Client) reçoit les propriétés du composant parent (Liste)
+//On reçoit les différentes propriétés sous forme d'objet json
+const Client = ({client}) => {
+
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{client.prenom} {client.nom}</h5>
+        <p className="card-text">  {client.societe} - {client.ca} </p>
+        <Link to="/" className="btn btn-primary">Voir</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Client;
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
+import Liste from './../components/Liste';
 
 const Home = () => {
     return ( 
         <>
             <Header />
-            <h1 className="container">page Home</h1>
+            <Liste />
             <Footer />
         </>
 
